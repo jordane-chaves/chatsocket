e7c6ea2e514b0c09bab799a3bf1f632de8f10e90
jordane-chaves <jordane_chaves@yahoo.com.br>
2022-12-28 18:50:33 -0300

    Update faker data

--- web/src/chat/faker-data.ts
+++ web/src/chat/faker-data.ts
@@ -1,21 +1,64 @@
 import { faker } from '@faker-js/faker';
 
+import { addToContactList } from './use-cases/add-to-contact-list';
+import { addMessage } from './use-cases/add-message';
+
 export function setFakerData() {
-  const johnDoeFakerImg = faker.image.avatar();
+  setUsersInfos();
+  setMessages();
+}
 
-  document
-    .querySelector('#contacts_list li:nth-child(2) img')
-    ?.setAttribute('src', johnDoeFakerImg);
+function setUsersInfos() {
+  addToContactList({
+    id: faker.datatype.uuid(),
+    name: faker.name.firstName(),
+    avatar: faker.image.avatar(),
+    email: faker.internet.email(),
+    socketId: faker.datatype.uuid(),
+  });
 
-  document
-    .querySelector('#messages_header img')
-    ?.setAttribute('src', johnDoeFakerImg);
+  addToContactList({
+    id: faker.datatype.uuid(),
+    name: faker.name.firstName(),
+    avatar: faker.image.avatar(),
+    email: faker.internet.email(),
+    socketId: faker.datatype.uuid(),
+  });
 
   document
     .querySelector('header .right .avatar')
     ?.setAttribute('src', faker.image.avatar());
+}
 
-  document
-    .querySelector('#contacts_list li:nth-child(1) img')
-    ?.setAttribute('src', faker.image.avatar());
+function setMessages() {
+  const messages = document.querySelector('.content main')!;
+  messages.classList.remove('hidden');
+
+  const messagesNumber = 10;
+
+  for (let i = 0; i < messagesNumber; i++) {
+    addMessage({
+      message: {
+        id: faker.datatype.uuid(),
+        text: faker.lorem.sentences(),
+        from: faker.datatype.uuid(),
+        roomId: faker.datatype.uuid(),
+        createdAt: new Date(),
+      },
+      user: {
+        id: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+        avatar: faker.image.avatar(),
+        email: faker.internet.email(),
+        socketId: faker.datatype.uuid(),
+      },
+      userLogged: {
+        id: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+        avatar: faker.image.avatar(),
+        email: faker.internet.email(),
+        socketId: faker.datatype.uuid(),
+      }
+    });
+  }
 }
